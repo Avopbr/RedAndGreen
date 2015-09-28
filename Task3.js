a6c5be87a60dba2f59fc5fca38afd88edd4212ce
Avopbr <abadwanguzeli@gmail.com>
2015-09-28 14:02:24 +0200

    Task3 is done

---
+++ Task3.js
@@ -0,0 +1,11 @@
+ var redbutton = document.getElementById('redButton');
+	  redButton.addEventListener('click', function () {
+	  var block = document.getElementById('block');
+	  block.className = "red"; 	
+	  });
+
+	  var greenbutton = document.getElementById('greenButton');
+	  greenButton.addEventListener('click', function () {
+	  var block = document.getElementById('block');
+	  block.className = "green"; 	
+	  });
