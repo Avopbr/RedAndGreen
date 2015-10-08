e42d8d2664b593d8cda1a7437a4625ce277d2a59
Avopbr <abadwanguzeli@gmail.com>
2015-10-08 16:28:05 +0200

    Adding my tasks

---
+++ Task3.js
@@ -0,0 +1,14 @@
+ var redbutton = document.getElementById('redButton');
+	  redButton.addEventListener('click', function () {
+      var block = document.getElementById('block');
+      block.innerHTML = "stop";
+	  block.className = "red"; 	
+	  });
+
+	  var greenbutton = document.getElementById('greenButton');
+	  greenButton.addEventListener('click', function () {
+	  var block = document.getElementById('block');
+	  block.innerHTML = "go"; 
+	  block.className = "green"; 
+	  });	
+	 
